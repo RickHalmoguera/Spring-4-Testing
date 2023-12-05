1c121ae2b9a45c1e670aa6b965dfae0f62c55401
Ricardo Halmoguera <halmoguerar@gmail.com>
2023-12-05 10:10:08 +0100

    is Ocuupied restored

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Room {
-    constructor(name, rate, discount, bookings) {
+    constructor(name,bookings, rate, discount ) {
         this.name = name
         this.bookings = bookings
         this.rate = rate
@@ -8,35 +8,34 @@ class Room {
 
     isOccupied(date) {
         const [day, month, year] = date.split('/').map(Number)
-        const inputDate = new Date(year, month - 1, day)
+        const [checkInDay, checkInMonth,checkInYear] =this.bookings.checkIn.split('/').map(Number)
+        const [checkOutDay, checkOutMonth,checkOutYear] =this.bookings.checkOut.split('/').map(Number)
+        const inputDate = new Date(Date.UTC(year, month - 1, day))
+        const checkInDateFormatted= new Date(Date.UTC(checkInYear, checkInMonth -1,checkInDay))
+        const checkOutDateFormatted= new Date(Date.UTC(checkOutYear, checkOutMonth -1,checkOutDay))
 
-        return this.bookings.some(booking => {
-            const [checkInDay, checkInMonth, checkInYear] = booking.checkIn.split('/').map(Number)
-            const [checkOutDay, checkOutMonth, checkOutYear] = booking.checkOut.split('/').map(Number)
+        
+        return inputDate >= checkInDateFormatted && inputDate <= checkOutDateFormatted
 
-            const checkInDate = new Date(checkInYear, checkInMonth - 1, checkInDay)
-            const checkOutDate = new Date(checkOutYear, checkOutMonth - 1, checkOutDay)
-
-            return checkInDate <= inputDate && inputDate <= checkOutDate
-        })
     }
+    
+    
 
-    occupancyPercentage(startDate, endDate){
-        const [startDateDay, startDateMonth, startDateYear] = startDate.split('/').map(Number)
-        const [endDateDay, endDateMonth, endDateYear] = endDate.split('/').map(Number)
-        const startDateFormatted= new Date(startDateYear, startDateMonth -1, startDateDay)
-        const endDateFormatted= new Date(endDateYear, endDateMonth -1, endDateDay)
-        const totalDays=(endDateFormatted - startDateFormatted) / (1000 * 60 * 60 * 24) + 1
+    occupancyPercentage(startDate, endDate) {
+        const [startDateDay, startDateMonth, startDateYear] = startDate.split('/').map(Number);
+        const [endDateDay, endDateMonth, endDateYear] = endDate.split('/').map(Number);
+        const startDateFormatted = new Date(startDateYear, startDateMonth - 1, startDateDay);
+        const endDateFormatted = new Date(endDateYear, endDateMonth - 1, endDateDay);
+        const totalDays = (endDateFormatted - startDateFormatted) / (1000 * 60 * 60 * 24) + 1;
         let roomOccupiedDays = 0;
-       
+
         this.bookings.forEach(booking => {
             const [checkInDay, checkInMonth, checkInYear] = booking.checkIn.split('/').map(Number);
             const [checkOutDay, checkOutMonth, checkOutYear] = booking.checkOut.split('/').map(Number);
-        
+
             const checkInDate = new Date(checkInYear, checkInMonth - 1, checkInDay);
             const checkOutDate = new Date(checkOutYear, checkOutMonth - 1, checkOutDay);
-        
-           
+
             if (checkInDate <= endDateFormatted && checkOutDate >= startDateFormatted) {
                 const overlapStartDate = Math.max(startDateFormatted, checkInDate);
                 const overlapEndDate = Math.min(endDateFormatted, checkOutDate);
@@ -44,12 +43,12 @@ class Room {
                 roomOccupiedDays += days;
             }
         });
-        return roomOccupiedDays * 100 / totalDays
 
+        return (roomOccupiedDays * 100) / totalDays;
     }
 
-    totalOccupancyPercentage(rooms, startDate, endDate) {
-        //
+    static totalOccupancyPercentage(rooms, startDate, endDate) {
+        return rooms
     }
     
 }
@@ -62,6 +61,13 @@ class Booking {
         this.checkOut = checkOut
         this.discount = discount
         this.rooms = rooms
+    }
+
+    getFee(rate,roomDiscount,bookingDiscount){
+        const rateInEuros = rate/100
+        const rateRoom = rateInEuros -(rateInEuros*roomDiscount/100)
+        const fullFee = rateRoom - ( rateRoom*bookingDiscount/100)
+        return Number(fullFee.toFixed(2))
     }
 }
 
--- index.test.js
+++ index.test.js
@@ -3,34 +3,73 @@ const roomsList = require('./JSON/rooms.json')
 const bookingsList = require('./JSON/bookings.json')
 
 
-describe ('Check room availability', ()=>{
+describe ('Check the room occupancy on a specific date', ()=>{
     
-    test('Check the availability of a room in 01/01/2023', () => {
-        const room1 = new Room(roomsList[0].name, [bookingsList[0]], roomsList[0].rate, roomsList[0].discount)
+    test('Check whether the booking from 10/01/2023 to 12/01/2023 is occupied on 01/01/2023', () => {
+        const room1 = new Room(
+            roomsList[0].name, 
+            bookingsList[0], 
+            roomsList[0].rate,
+            roomsList[0].discount
+        )
         expect(room1.isOccupied('01/01/2023')).toBeFalsy()
     })
-    
-    test('Check the availability of a room in 11/01/2023', () => {
-        const room1 = new Room(roomsList[0].name, [bookingsList[0]], roomsList[0].rate, roomsList[0].discount)
+   
+    test('Check whether the booking from 10/01/2023 to 12/01/2023 is occupied on 11/01/2023', () => {
+        const room1 = new Room(
+            roomsList[0].name, 
+            bookingsList[0], 
+            roomsList[0].rate, 
+            roomsList[0].discount
+        )
         expect(room1.isOccupied('11/01/2023')).toBeTruthy()
     })
 
-    test('Check the availability of a room in 15/01/2023', () => {
-        const room1 = new Room(roomsList[0].name, [bookingsList[0]], roomsList[0].rate, roomsList[0].discount)
+    test('Check whether the booking from 10/01/2023 to 12/01/2023 is occupied on 15/01/2023', () => {
+        const room1 = new Room(
+            roomsList[0].name, 
+            bookingsList[0], 
+            roomsList[0].rate, 
+            roomsList[0].discount
+        )
         expect(room1.isOccupied('15/01/2023')).toBeFalsy()
     })
 
 
 })
 
-describe ('Check the percentage of occupancy a room', ()=>{
-    test('Check the percentage of occupancy between 01/01/2022 and 31/12/2022', ()=>{
+/*
+describe ('Check the percentage of room occupancy on a specific date', ()=>{
+    test('Check the percentage of occupancy between 01/01/2022 and 31/12/2022 of the Room 0', ()=>{
         const room1 = new Room(roomsList[0].name,[...bookingsList], roomsList[0].rate, roomsList[0].discount )
         expect(room1.occupancyPercentage("01/01/2022","31/12/2022")).toBe(0)
     })
 
-    test('Check the percentage of occupancy between 01/01/2023 and 31/12/2023', ()=>{
+    test('Check the percentage of occupancy between 01/01/2023 and 31/12/2023 of the Room 0', ()=>{
         const room1 = new Room(roomsList[0].name,[...bookingsList], roomsList[0].rate, roomsList[0].discount)
         expect(room1.occupancyPercentage("01/01/2023","31/12/2023")).toBeGreaterThan(0)
     })
 })
+
+describe('Check the total occupancy of the hotel in a time period', ()=>{
+    test('Check the percentage of the occupancy between 01/01/2022 and 31/12/2022 of the whole hotel',()=>{
+        const rooms = 
+        expect(Room.totalOccupancyPercentage(roomsList,"01/01/2022","31/12/2022")).toBe(0)
+    })
+})
+
+
+
+describe('Check fee ',()=>{
+    test('Check that room 0 have a total fee of ',()=>{
+        const booking1= new Booking(
+            bookingsList[0].name, 
+            bookingsList[0].email, 
+            bookingsList[0].checkIn, 
+            bookingsList[0].checkOut,
+            bookingsList[0].discount,
+            roomsList[0]
+        )
+        expect(booking1.getFee(booking1.rooms.rate, booking1.rooms.discount, booking1.discount)).toBe(12.42)
+    })
+})*/
